0728cfaf371c8d29e0348588a6bb2835146e955a
Mariusz Tang <dev@mariusztang.com>
2025-03-05 01:48:11 +0000

    add wezterm

---
+++ eggs/wezterm/colors.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local colors = {}
+
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local is_dark_appearance = get_appearance():find("Dark")
+
+colors.scheme = is_dark_appearance and "GruvboxDark" or "GruvboxLight"
+colors.scheme_object = wezterm.color.get_builtin_schemes()[colors.scheme]
+colors.tab_bar_bg_color = is_dark_appearance and "#3c3836" or "ebdbb2"
+colors.tab_bar_inactive_fg_color = is_dark_appearance and "#a89984" or "#7c6f64"
+colors.window_bg_opacity = is_dark_appearance and 0.2 or 0.4
+
+return colors
