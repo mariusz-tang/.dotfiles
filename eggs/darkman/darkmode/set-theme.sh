ba7999cff32306e0a6abfeae71608d9509a7d4bf
Mariusz Tang <dev@mariusztang.com>
2025-03-06 13:14:28 +0000

    add darkman

---
+++ eggs/darkman/darkmode/set-theme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+xset-gtk-theme Adwaita-dark
+THEME=dark-colors start-polybar
+feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/dark.jpg
---
+++ eggs/darkman/lightmode/set-theme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+xset-gtk-theme Adwaita
+THEME=light-colors start-polybar
+feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/light.png
