07797b1d355701b5ab9e8548ad3fb59f98e7615f
Mariusz Tang <dev@mariusztang.com>
2025-03-07 23:12:46 +0000

    nvim: add missing plugins

---
+++ eggs/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,8 @@
+return {
+  "catgoose/nvim-colorizer.lua",
+  event = "VeryLazy",
+  opts = {
+    lazy_load = true,
+    user_default_options = { names = false, css = { css = true } },
+  },
+}
---
+++ eggs/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  "ellisonleao/glow.nvim",
+  opts = {
+    border = "none",
+    height_ratio = 0.8,
+    width_ratio = 0.9,
+  },
+  keys = {
+    { "<leader>pm", "<cmd>Glow<cr>", desc = "[P]review [M]arkdown" },
+  },
+}
