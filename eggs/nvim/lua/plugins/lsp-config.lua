e89655e2c52e75c2f79461b31a2e4456267fb41d
Mariusz Tang <dev@mariusztang.com>
2025-04-04 17:39:38 +0100

    nvim: display diagnostics using virtual text

--- eggs/nvim/lua/plugins/lsp-config.lua
+++ eggs/nvim/lua/plugins/lsp-config.lua
@@ -68,7 +68,7 @@ return {
       for type, icon in pairs(signs) do
         diagnostic_signs[vim.diagnostic.severity[type]] = icon
       end
-      vim.diagnostic.config({ signs = { text = diagnostic_signs } })
+      vim.diagnostic.config({ signs = { text = diagnostic_signs }, virtual_text = true })
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
