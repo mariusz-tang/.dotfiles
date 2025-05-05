dbe03d6a044d2847b6de877d38f5e4d7a8239b3e
Mariusz Tang <dev@mariusztang.com>
2025-05-05 22:39:29 +0100

    neovim: show treesitter context

--- eggs/nvim/lua/plugins/treesitter.lua
+++ eggs/nvim/lua/plugins/treesitter.lua
@@ -34,4 +34,5 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  "nvim-treesitter/nvim-treesitter-context",
 }
