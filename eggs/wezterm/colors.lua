b41901d5ee89daf1d3f5cd8f0847fc168fbf077c
Mariusz Tang <dev@mariusztang.com>
2025-06-29 13:47:40 +0100

    add gaps and remove transparency

--- eggs/wezterm/colors.lua
+++ eggs/wezterm/colors.lua
@@ -14,6 +14,5 @@ colors.scheme = is_dark_appearance and "GruvboxDark" or "GruvboxLight"
 colors.scheme_object = wezterm.color.get_builtin_schemes()[colors.scheme]
 colors.tab_bar_bg_color = is_dark_appearance and "#3c3836" or "ebdbb2"
 colors.tab_bar_inactive_fg_color = is_dark_appearance and "#a89984" or "#7c6f64"
-colors.window_bg_opacity = 0.5
 
 return colors
