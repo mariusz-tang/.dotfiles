4207585be9045a9a035fc4f31644ce24a0271394
Mariusz Tang <dev@mariusztang.com>
2025-09-04 01:59:33 +0100

    nvim: reduce plugin update check frequency
    Reduce the frequency at which lazy.nvim checks for plugin updates from
    once per hour to once per day.

--- eggs/nvim/lua/config/lazy.lua
+++ eggs/nvim/lua/config/lazy.lua
@@ -24,6 +24,9 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox", "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- Automatically check for plugin updates once a day.
+  checker = {
+    enabled = true,
+    frequency = 86400,
+  },
 })
