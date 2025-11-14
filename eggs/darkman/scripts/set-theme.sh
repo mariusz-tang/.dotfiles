94c2643d4585913e3deb4a02571a7fae6c99cdb8
Mariusz Tang <dev@mariusztang.com>
2025-11-14 21:25:24 +0000

    darkman: set the correct gtk theme

--- eggs/darkman/scripts/set-theme.sh
+++ eggs/darkman/scripts/set-theme.sh
@@ -2,7 +2,11 @@
 
 theme_name=$1
 
-xset-gtk-theme Adwaita-dark
+if [[ $theme_name = dark ]]; then
+    gtk_suffix='-dark'
+fi
+
+xset-gtk-theme Adwaita${gtk_suffix}
 sed 's/inherit = \(dark\|light\)-colors/inherit = '${theme_name}'-colors/' --in-place ~/.config/yolk/eggs/polybar/polybar/config.ini
 feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/${theme_name}
 sed 's+general.import = \[ "themes/themes/gruvbox_\(dark\|light\).toml" \]+general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]+' --in-place ~/.config/alacritty/alacritty.toml
