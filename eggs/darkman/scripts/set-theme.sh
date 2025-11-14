46df9ea603558cbf36a5762320ba4996101f048a
Mariusz Tang <dev@mariusztang.com>
2025-11-14 21:10:06 +0000

    darkman: update script to directly edit configs
    Previously the script would create files that were imported by the
    relevant configs but this is annoying to repeat across every affected
    program.
    
    It is less annoying (and cleaner) to directly edit the relevant config
    files using sed.

--- eggs/darkman/scripts/set-theme.sh
+++ eggs/darkman/scripts/set-theme.sh
@@ -5,5 +5,5 @@ theme_name=$1
 xset-gtk-theme Adwaita-dark
 sed 's/inherit = \(dark\|light\)-colors/inherit = '${theme_name}'-colors/' --in-place ~/.config/yolk/eggs/polybar/polybar/config.ini
 feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/${theme_name}
-echo 'general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]' > ~/.config/alacritty/theme.toml
-echo '@import "colors/'${theme_name}'"' > ~/.config/rofi/colors/light-dark.rasi
+sed 's+general.import = \[ "themes/themes/gruvbox_\(dark\|light\).toml" \]+general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]+' --in-place ~/.config/alacritty/alacritty.toml
+sed 's+@import "colors/\(dark\|light\)"+@import "colors/'${theme_name}'"+' --in-place ~/.config/yolk/eggs/rofi/config/common.rasi
