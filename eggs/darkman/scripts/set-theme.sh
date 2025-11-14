d0a27810e3c3aa1cdbe0d0c826a75eecaef9c81e
Mariusz Tang <dev@mariusztang.com>
2025-11-14 20:54:00 +0000

    polybar: remove environment variable theme check
    We now change the theme by directly editing the config file. The new
    darkman script does it automatically.

--- eggs/darkman/scripts/set-theme.sh
+++ eggs/darkman/scripts/set-theme.sh
@@ -3,7 +3,7 @@
 theme_name=$1
 
 xset-gtk-theme Adwaita-dark
-THEME=${theme_name}-colors start-polybar &
+sed 's/inherit = \(dark\|light\)-colors/inherit = '${theme_name}'-colors/' --in-place ~/.config/yolk/eggs/polybar/polybar/config.ini
 feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/${theme_name}
 echo 'general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]' > ~/.config/alacritty/theme.toml
 echo '@import "colors/'${theme_name}'"' > ~/.config/rofi/colors/light-dark.rasi
