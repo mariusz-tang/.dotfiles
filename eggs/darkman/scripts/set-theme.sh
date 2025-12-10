788d949f7333b60d5f19014074f967fd268bf546
Mariusz Tang <dev@mariusztang.com>
2025-12-10 00:50:23 +0000

    dunst: update colors to match system theme

--- eggs/darkman/scripts/set-theme.sh
+++ eggs/darkman/scripts/set-theme.sh
@@ -4,6 +4,16 @@ theme_name=$1
 
 if [[ $theme_name = dark ]]; then
     gtk_suffix='-dark'
+
+    sed 's/background = "#[0-9a-f]\{6\}"/background = "#282828"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/foreground = "#[0-9a-f]\{6\}"/foreground = "#fbf1c7"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/highlight = "#[0-9a-f]\{6\}"/highlight = "#fabd2f"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/frame_color = "#[0-9a-f]\{6\}"/frame_color = "#fabd2f"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+else
+    sed 's/background = "#[0-9a-f]\{6\}"/background = "#fbf1c7"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/foreground = "#[0-9a-f]\{6\}"/foreground = "#282828"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/highlight = "#[0-9a-f]\{6\}"/highlight = "#b57614"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    sed 's/frame_color = "#[0-9a-f]\{6\}"/frame_color = "#b57614"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
 fi
 
 xset-gtk-theme Adwaita${gtk_suffix}
@@ -11,3 +21,4 @@ sed 's/inherit = \(dark\|light\)-colors/inherit = '${theme_name}'-colors/' --in-
 feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/${theme_name}
 sed 's+general.import = \[ "themes/themes/gruvbox_\(dark\|light\).toml" \]+general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]+' --in-place ~/.config/alacritty/alacritty.toml
 sed 's+@import "colors/\(dark\|light\)"+@import "colors/'${theme_name}'"+' --in-place ~/.config/yolk/eggs/rofi/config/common.rasi
+dunstctl reload
