e25e6cee1ec719c8ea2ec2bfd634aa8eb79ae765
Mariusz Tang <dev@mariusztang.com>
2025-12-12 01:34:22 +0000

    darkman: clean up set-theme script
    - clearly indicate which parts affect which applications.
    - move repeated dunst config edits out of if-statement.
    - factor sed invocations into a function.

--- eggs/darkman/scripts/set-theme.sh
+++ eggs/darkman/scripts/set-theme.sh
@@ -4,21 +4,47 @@ theme_name=$1
 
 if [[ $theme_name = dark ]]; then
     gtk_suffix='-dark'
-
-    sed 's/background = "#[0-9a-f]\{6\}"/background = "#282828"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/foreground = "#[0-9a-f]\{6\}"/foreground = "#fbf1c7"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/highlight = "#[0-9a-f]\{6\}"/highlight = "#fabd2f"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/frame_color = "#[0-9a-f]\{6\}"/frame_color = "#fabd2f"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    dunst_bg='#282828'
+    dunst_fg='#fbf1c7'
+    dunst_accent='#fabd2f'
 else
-    sed 's/background = "#[0-9a-f]\{6\}"/background = "#fbf1c7"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/foreground = "#[0-9a-f]\{6\}"/foreground = "#282828"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/highlight = "#[0-9a-f]\{6\}"/highlight = "#b57614"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
-    sed 's/frame_color = "#[0-9a-f]\{6\}"/frame_color = "#b57614"/' --in-place ~/.config/yolk/eggs/dunst/dunstrc
+    dunst_bg='#fbf1c7'
+    dunst_fg='#282828'
+    dunst_accent='#b57614'
 fi
 
+function replace {
+    original=$1
+    target=$2
+    file=$3
+    delimiter=$4
+    if [[ $delimiter == '' ]]; then
+        delimiter='/'
+    fi
+    sed 's'${delimiter}${original}${delimiter}${target}${delimiter} --in-place ${file}
+}
+
+# GTK theme.
 xset-gtk-theme Adwaita${gtk_suffix}
-sed 's/inherit = \(dark\|light\)-colors/inherit = '${theme_name}'-colors/' --in-place ~/.config/yolk/eggs/polybar/polybar/config.ini
+
+# Background.
 feh --bg-scale $(xdg-user-dir PICTURES)/backgrounds/${theme_name}
-sed 's+general.import = \[ "themes/themes/gruvbox_\(dark\|light\).toml" \]+general.import = [ "themes/themes/gruvbox_'${theme_name}'.toml" ]+' --in-place ~/.config/alacritty/alacritty.toml
-sed 's+@import "colors/\(dark\|light\)"+@import "colors/'${theme_name}'"+' --in-place ~/.config/yolk/eggs/rofi/config/common.rasi
+
+# Polybar.
+replace 'inherit = \(dark\|light\)-colors' 'inherit = '${theme_name}'-colors' ~/.config/polybar/config.ini
+
+# Alacritty.
+replace 'gruvbox_\(dark\|light\).toml' 'gruvbox_'${theme_name}'.toml' ~/.config/alacritty/alacritty.toml +
+
+# Rofi.
+replace '@import "colors/\(dark\|light\)"' '@import "colors/'${theme_name}'"' ~/.config/rofi/common.rasi +
+
+# Picom.
+replace 'unfocused-shader-\(light\|dark\)\.glsl' 'unfocused-shader-'${theme_name}'.glsl'  ~/.config/picom/picom.conf
+
+# Dunst.
+replace 'background = "#[0-9a-f]\{6\}"' 'background = "'${dunst_bg}'"' ~/.config/dunst/dunstrc
+replace 'foreground = "#[0-9a-f]\{6\}"' 'foreground = "'${dunst_fg}'"' ~/.config/dunst/dunstrc
+replace 'highlight = "#[0-9a-f]\{6\}"' 'highlight = "'${dunst_accent}'"' ~/.config/dunst/dunstrc
+replace 'frame_color = "#[0-9a-f]\{6\}"' 'frame_color = "'${dunst_accent}'"' ~/.config/dunst/dunstrc
 dunstctl reload
