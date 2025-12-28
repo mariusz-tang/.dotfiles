914dcb54966b85891235268e12ce6f0820ebea58
Mariusz Tang <dev@mariusztang.com>
2025-12-28 23:42:18 +0000

    nvim: send project settings notification on start

--- eggs/nvim/lua/project-settings/load.lua
+++ eggs/nvim/lua/project-settings/load.lua
@@ -1,15 +1,20 @@
 -- Load the correct project settings module.
 local settings_module = "project-settings.default"
+local notification_text = "Loaded default project settings!"
 local cwd = vim.uv.cwd() or ""
-if string.match(cwd, "^" .. vim.env.HOME .. "/.config/yolk") then
+local in_dotfile_directory = string.match(cwd, "^" .. vim.env.HOME .. "/.config/yolk")
+if in_dotfile_directory then
   settings_module = "project-settings.dotfiles"
+  notification_text = "Loaded dotfiles project settings!"
 else
   local has_project_map, project_map = pcall(require, "project-settings.map")
-  if has_project_map then
-    settings_module = project_map[cwd] or settings_module
+  if has_project_map and project_map[cwd] then
+    settings_module = project_map[cwd]
+    notification_text = "Loaded custom project settings!"
   end
 end
 local settings = require(settings_module)
+settings.notification_text = notification_text
 
 -- Insert missing tables.
 settings.lsps = settings.lsps or {}
