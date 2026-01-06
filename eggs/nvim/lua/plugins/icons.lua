ed6022b1e510e4c13819df0a4c490a3d48e26ccd
Mariusz Tang <dev@mariusztang.com>
2026-01-06 00:02:50 +0000

    nvim: add lsp icons

--- eggs/nvim/lua/plugins/icons.lua
+++ eggs/nvim/lua/plugins/icons.lua
@@ -1,6 +1,11 @@
-return { {
-  "nvim-mini/mini.icons",
-  version = false,
-  lazy = true,
-  opts = {},
-} }
+return {
+  {
+    "nvim-mini/mini.icons",
+    version = false,
+    lazy = true,
+    config = function()
+      require("mini.icons").setup()
+      MiniIcons.tweak_lsp_kind()
+    end,
+  },
+}
