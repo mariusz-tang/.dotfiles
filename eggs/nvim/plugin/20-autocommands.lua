74c05e46edd1ee8edc546068c03008d78791ffdf
Mariusz Tang <dev@mariusztang.com>
2026-04-23 00:48:26 +0100

    nvim: enable treesitter for gitconfig files

--- eggs/nvim/plugin/20-autocommands.lua
+++ eggs/nvim/plugin/20-autocommands.lua
@@ -4,6 +4,11 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "wincmd L",
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitconfig",
+  command = "lua vim.treesitter.start()",
+})
+
 -- Auto resize splits when the terminal's window is resized.
 vim.api.nvim_create_autocmd("VimResized", {
   command = "wincmd =",
