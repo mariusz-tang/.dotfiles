78079e65ac3031ef0b4f7206a5d762cb337f2540
Mariusz Tang <dev@mariusztang.com>
2026-08-12 23:43:21 +0100

    nvim: add cmake lsp and formatter

--- eggs/nvim/init.lua
+++ eggs/nvim/init.lua
@@ -10,6 +10,7 @@ MyLsps = {
   "html",
   "jsonls",
   "lua_ls",
+  "neocmake",
   "pylsp",
   "tombi",
   "yamlls",
@@ -20,6 +21,7 @@ MyConformOpts = {
     lsp_format = "fallback",
   },
   formatters_by_ft = {
+    cmake = { "gersemi" },
     lua = { "stylua" },
     zsh = { "beautysh" },
   },
