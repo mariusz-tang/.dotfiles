7e20839a3e803dde3f86ebdc60f8dd516533f1c0
Mariusz Tang <dev@mariusztang.com>
2026-08-12 23:44:07 +0100

    nvim: add gdb dap configuration

--- eggs/nvim/plugin/dap.lua
+++ eggs/nvim/plugin/dap.lua
@@ -7,8 +7,29 @@ vim.pack.add({
 
 -- NVIM DAP -------------------------------------------------------------------
 local dap = require("dap")
-require("dap-python").setup("/usr/sbin/python")
 dap.defaults.fallback.switchbuf = "usevisible,usetab,newtab"
+
+-- Python
+require("dap-python").setup("/usr/sbin/python")
+
+-- C++
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+}
 
 if SetupDap then
   SetupDap(dap)
